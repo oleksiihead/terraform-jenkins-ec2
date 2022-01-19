e9de32ecc70fa836ec7202600ff6424e2c503a33
Oleksii Smiichuk <smiychuk@gmail.com>
2022-01-19 09:43:30 +0200

    Add ssh_clone_url output

--- terraform/github_repo/outputs.tf
+++ terraform/github_repo/outputs.tf
@@ -1,3 +1,9 @@
 output "repo_url" {
   value = github_repository.repo.html_url
+  description = "URL to the repository on the web."
+}
+
+output "ssh_clone_url" {
+  value = github_repository.repo.ssh_clone_url
+  description = "URL that can be provided to git clone to clone the repository via SSH."
 }
